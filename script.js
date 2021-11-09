251af3abcabb93d9cf43603ea890297020795084
Kasteish <by.zycko@gmail.com>
2021-11-09 13:17:48 -0300

    Up

---
+++ script.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+const { v4: uuid } = require("uuid");
+const http = require ("http");
+http.createServer((res, req)=>{
+
+}).listen(8080, ()=>console.log("Puerto 8080 conectado mi rey"))
